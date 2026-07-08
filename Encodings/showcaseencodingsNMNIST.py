a6e7b54df1f1baac5ae9722b71e809c49e9e6afd
Kaleb Keichel <kk471921@ohio.edu>
2026-07-08 01:44:42 -0400

    Refactored encodings code. Started implementing CIFAR10DVS dataset (ongoing)

---
+++ Encodings/showcaseencodingsNMNIST.py
@@ -0,0 +1,143 @@
+import tonic
+from tonic import transforms
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+import numpy as np
+import sys 
+import argparse
+
+import nmnistencodings as encodings
+
+
+
+### Command line arguments
+debug = False
+plot = False
+encoding = ""
+checkpoint_file = ""
+
+# setup args
+parser = argparse.ArgumentParser(description="Training for NMNIST SNN models.")
+
+parser.add_argument("encoding", type=int, help="Encoding type. 0: spiketrain, 1: voxel grids, 2: DCT, 3: truncated DCT, 4: aggressive DCT")
+parser.add_argument("-d", "--debug", action="store_true", help="Enable debug output")
+parser.add_argument("-p", "--plot", action="store_true", help="Enable plotting after training")
+
+# get args
+args = parser.parse_args()
+
+# parse args
+
+# Encodings
+# Encodings
+if(args.encoding > 4):
+    sys.exit(f"Error, incorrect encoding type: {args.encoding}")
+if(args.encoding == 0): 
+    transform = encodings.spiketrain_transform
+    encoding = "spike_train"
+    checkpoint_file = "SpikeTrain"
+elif(args.encoding == 1):
+    transform = encodings.voxel_grids_transform
+    encoding = "voxel_grid"
+    checkpoint_file = "VoxelGrids"
+elif(args.encoding == 2):
+    transform = encodings.dct_transform
+    encoding = "dct"
+    checkpoint_file = "DCT"
+elif(args.encoding == 3):
+    transform = encodings.truncated_dct_transform
+    encoding = "trunc_dct"
+    checkpoint_file = "TruncatedDCT"
+elif(args.encoding == 4):
+    transform = encodings.aggressive_dct_transform
+    encoding = "aggr_dct"
+    checkpoint_file = "AggressiveDCT"
+
+# Debug and plotting
+if(args.debug == True):
+    debug = True
+if(args.plot == True):
+    plot = True
+
+# Translate to frame
+
+
+# Load the dataset and encode
+raw_dataset = tonic.datasets.NMNIST(
+    save_to="../Datasets/NMNIST/",
+    train=True
+)
+
+train_dataset = tonic.datasets.NMNIST(
+    save_to="../Datasets/NMNIST/",
+    train=True,
+    transform=transform
+)
+
+test_dataset = tonic.datasets.NMNIST(
+    save_to="../Datasets/NMNIST/",
+    train=False,
+    transform=transform
+)
+
+# Let's do some debug output
+if(debug): 
+
+    print("Dataset information")
+    frames, label = train_dataset[0]
+    print("dtype:", frames.dtype)
+    print("shape:", frames.shape)
+    print("min:", frames.min())
+    print("max:", frames.max())
+    print("nonzero:", (frames > 0).sum())
+
+    vals = np.unique(frames)
+
+    print(vals[:20])
+    print("count:", len(vals))
+    
+
+
+if plot:
+    print("Generating images")
+    for t in range(10):
+        plt.figure()
+        plt.imshow(frames[t,0])
+        plt.title(f"Timestep {t}")
+        # plt.show()
+        plt.savefig(f"../Plots/NMNIST/{checkpoint_file}/{encoding}_timestep{t}")
+    summed = frames.sum(axis=0)
+
+    plt.figure()
+    plt.imshow(summed[0] + summed[1])
+    plt.title("Summed Digit")
+    plt.savefig(f"../Plots/NMNIST/{checkpoint_file}/{encoding}_summed_digit")
+
+    print(f"Saving plots to ../Plots/NMNIST/{checkpoint_file}/")
+
+print(f"Encoding: {encoding}")
+print(f"Shape = {train_dataset[0][0].shape}")
+print("Window = 1000 μs")
+
+# Create dataloaders
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=32,
+    shuffle=True
+)
+
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=32,
+    shuffle=False
+)
+
+# Debug 
+if(debug):
+    frames, labels = next(iter(train_loader))
+
+    print("Train dataloader debug")
+    print(f"\tFrame shape: {frames.shape}")
+    print(f"\tFrame dtype: {frames.dtype}")
+    print(f"\tFrame min: {frames.min()}")
+    print(f"\tFrame max: {frames.max()}")
--- Training/trainhaar.sh
+++ Training/trainhaar.sh
@@ -2,33 +2,15 @@
 
 # paths
 VENV_PATH="../.venv"
-LOG_DIR="../Logs/Training"
+LOG_DIR="../Logs/Training/FrontEndWaveletSNN"
 
 # activate env
 source "$VENV_PATH/bin/activate"
 
 
-# encoding 1: spike train
-ENC1DATESTAMP=$(date +"%m%d%y")
-ENC1TIMESTAMP=$(date +"%H%M")
-python trainwavelet.py > "$LOG_DIR/HaarVoxelGrids/voxelgrid_${ENC1DATESTAMP}_${ENC1TIMESTAMP}.log" 2> "$LOG_DIR/HaarVoxelGrids/voxelgrid_${ENC1DATESTAMP}_${ENC1TIMESTAMP}.err"
-
-# encoding 2: voxel grids
-# ENC2DATESTAMP=$(date +"%m%d%y")
-# ENC2TIMESTAMP=$(date +"%H%M")
-# python trainvoxelgrid.py > "$LOG_DIR/VoxelGridsvoxelgrid_${ENC2DATESTAMP}_${ENC2TIMESTAMP}.log" 2> "$LOG_DIR/VoxelGridsvoxelgrid_${ENC2DATESTAMP}_${ENC2TIMESTAMP}.err"
-
-# encoding 3: DCT
-# ENC3DATESTAMP=$(date +"%m%d%y")
-# ENC3TIMESTAMP=$(date +"%H%M")
-# python traindct.py > "$LOG_DIR/DCT/dct_${ENC3DATESTAMP}_${ENC3TIMESTAMP}.log" 2> "$LOG_DIR/DCT/dct_${ENC3DATESTAMP}_${ENC3TIMESTAMP}.err"
-
-# encoding 4: Truncated DCT
-# ENC4DATESTAMP=$(date +"%m%d%y")
-# ENC4TIMESTAMP=$(date +"%H%M")
-# python traintruncdct.py > "$LOG_DIR/TruncatedDCT/truncdct_${ENC4DATESTAMP}_${ENC4TIMESTAMP}.log" 2> "$LOG_DIR/TruncatedDCT/truncdct_${ENC4DATESTAMP}_${ENC4TIMESTAMP}.err"
-
-# encoding 5: Aggressive DCT
-# ENC5DATESTAMP=$(date +"%m%d%y")
-# ENC5TIMESTAMP=$(date +"%H%M")
-# python trainaggrdct.py > "$LOG_DIR/AggressiveDCT/aggrdct_${ENC5DATESTAMP}_${ENC5TIMESTAMP}.log" 2> "$LOG_DIR/AggressiveDCT/aggrdct_${ENC5DATESTAMP}_${ENC5TIMESTAMP}.err"
+# encodings
+for i in {0..4}; do
+    ENCDATESTAMP=$(date +"%m%d%y")
+    ENCTIMESTAMP=$(date +"%H%M")
+    python trainNMNIST.py $i 1 4 -d > "$LOG_DIR/Full_${ENCDATESTAMP}_${ENCTIMESTAMP}.log" 2> "$LOG_DIR/Full_${ENCDATESTAMP}_${ENCTIMESTAMP}.err"
+done
